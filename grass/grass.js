5fec68fa7c9a955f82f9cb3f6fd2623b948ea31b
Flyspeck101 <74536088+Flyspeck101@users.noreply.github.com>
2023-05-10 20:51:21 +1000

    ok

--- grass/grass.js
+++ grass/grass.js
@@ -1,3 +1,15 @@
 let grass = (document) => {
-  
+  function hide(element) {
+    element.classList.add("hidden");
+  }
+
+  function show(element) {
+    element.classList.remove("hidden")
+  }
+
+  document.getElementsByTagName("body")[0].onloadend = function () {
+    document.getElementsByClassName("whole-page-modal")[0].innerHTML += `<br><br><br><p class="sans" style="font-size: 30px;">This game is designed for landscape screens. The game may function on mobile or portrait screens, but is not supported.</p><br><br><br><br><br><button id="done">Okay!</button>`;
+    document.getElementById("done").onclick = function () {document.getElementsByClassName("whole-page-modal")[0].innerHTML = ""; hide(document.getElementsByClassName("whole-page-modal")[0]); };
+  }; 
+  show(document.getElementsByClassName("whole-page-modal")[0]);
 }
