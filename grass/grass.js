25de3736ced795b94c9824fcf6ec677479131788
Flyspeck101 <yeetersdeleters12345@gmail.com>
2023-06-05 09:56:14 +1000

    Update grass.js

--- grass/grass.js
+++ grass/grass.js
@@ -8,7 +8,7 @@ let grass = (document) => {
   }
 
   document.getElementsByTagName("body")[0].onloadend = function () {
-    document.getElementsByClassName("whole-page-modal")[0].innerHTML += `<br><br><br><p class="sans" style="font-size: 30px;">This game is designed for landscape screens. The game may function on mobile or portrait screens, but is not supported.</p><br><br><br><br><br><button id="done">Okay!</button>`;
+    document.getElementsByClassName("whole-page-modal")[0].innerHTML = `<br><br><br><p class="sans" style="font-size: 30px;">This game is designed for landscape screens. The game may function on mobile or portrait screens, but is not supported.</p><br><br><br><br><br><button id="done">Okay!</button>`;
     document.getElementById("done").onclick = function () {document.getElementsByClassName("whole-page-modal")[0].innerHTML = ""; hide(document.getElementsByClassName("whole-page-modal")[0]); };
   }; 
   show(document.getElementsByClassName("whole-page-modal")[0]);
