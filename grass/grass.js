bd1bc522331baf7779fdddcead01624406f57d9f
Flyspeck101 <lawrence.chen8@education.nsw.gov.au>
2023-06-05 12:38:53 +1000

    Intro sequence v1

--- grass/grass.js
+++ grass/grass.js
@@ -1,15 +1,34 @@
 let grass = (document) => {
+  function toggleDarkMode() {
+    $("body")[0].classList.contains("dark-mode") ? $("body")[0].classList.remove("dark-mode") : $("body")[0].classList.add("dark-mode");
+  }
+
+  toggleDarkMode();
+
   function hide(element) {
     element.classList.add("hidden");
   }
 
   function show(element) {
-    element.classList.remove("hidden")
+    element.classList.remove("hidden");
   }
 
-  document.getElementsByTagName("body")[0].onloadend = function () {
-    document.getElementsByClassName("whole-page-modal")[0].innerHTML = `<br><br><br><p class="sans" style="font-size: 30px;">This game is designed for landscape screens. The game may function on mobile or portrait screens, but is not supported.</p><br><br><br><br><br><button id="done">Okay!</button>`;
-    document.getElementById("done").onclick = function () {document.getElementsByClassName("whole-page-modal")[0].innerHTML = ""; hide(document.getElementsByClassName("whole-page-modal")[0]); };
-  }; 
-  show(document.getElementsByClassName("whole-page-modal")[0]);
+  function gameBegin() {
+    $(".whole-page-modal").html("");
+    $(".whole-page-modal").hide();
+    $("audio")[0].play();
+  }
+
+  $(".whole-page-modal").html(`<p class="sans" style="font-size: 30px; margin-top: calc(50vh - 185px); margin-left: 15vw; margin-right: 15vw;">This game is designed for landscape screens. The game may function on mobile or portrait screens, but is not supported.</p><br><br><br><br><br><button id="done" class="serif">Okay!</button>`);
+  $("#done").on("click", function () {
+    $(".whole-page-modal").fadeOut(2000, function() {
+      $(".whole-page-modal").html(`<img src="./img/gameover.png" style="width: 50vw; margin: 50px 25vw;">`);
+      $(".whole-page-modal").fadeIn(3000, function() {
+        $(".whole-page-modal").html(`<img src="./img/gameover.png" style="width: 50vw; margin: 50px 25vw;"><button id="outside" class="serif">Go outside</button>`);
+        $("#outside").on("click", gameBegin);
+      });
+    });
+  });
+  show($(".whole-page-modal")[0]);
+
 }
